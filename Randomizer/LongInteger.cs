b8ef657e59abf12107b372eb62ea4497315bf750
DHEERAJ KUMAR <98643857+CYB3RPHO3NIX@users.noreply.github.com>
2023-09-26 23:59:26 +0530

    Added LongInteger as well

--- Randomizer/LongInteger.cs
+++ Randomizer/LongInteger.cs
@@ -4,19 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace Randomizer
 {
-    public static class Integer
+    public class LongInteger
     {
         /// <summary>
-        /// Generates a random integer within the full range of Int32 values.
+        /// Generates a random integer within the full range of Int64 values.
         /// </summary>
         /// <returns>A random integer.</returns>
-        public static int GetRandomInteger()
+        public static long GetRandomLong()
         {
             Random r = new Random();
-            return r.Next(Int32.MinValue, Int32.MaxValue);
+            return r.NextInt64(Int64.MinValue, Int64.MaxValue);
         }
 
         /// <summary>
@@ -25,14 +24,15 @@ namespace Randomizer
         /// <param name="minValue">The minimum value (inclusive).</param>
         /// <param name="maxValue">The maximum value (exclusive).</param>
         /// <returns>A random integer within the specified range.</returns>
-        public static int GetRandomInteger(int minValue, int maxValue)
+        public static long GetRandomLong(long minValue, long maxValue)
         {
             if (minValue >= maxValue)
             {
                 throw new ArgumentException("minValue must be less than maxValue");
             }
+
             Random r = new Random();
-            return r.Next(minValue, maxValue);
+            return r.NextInt64(minValue, maxValue);
         }
 
         /// <summary>
@@ -40,14 +40,14 @@ namespace Randomizer
         /// </summary>
         /// <param name="val">The minimum value (inclusive).</param>
         /// <returns>A random integer greater than or equal to the specified value.</returns>
-        public static int GetRandomIntegerGreaterThanOrEqualTo(int val)
+        public static long GetRandomLongGreaterThanOrEqualTo(long val)
         {
-            if (val == Int32.MaxValue)
+            if (val == Int64.MaxValue)
             {
-                throw new ArgumentOutOfRangeException("val", "value must be smaller than MaxValue of Int32");
+                throw new ArgumentOutOfRangeException("val", "value must be smaller than MaxValue of Int64");
             }
             Random r = new Random();
-            return r.Next(val, Int32.MaxValue);
+            return r.NextInt64(val, Int64.MaxValue);
         }
 
         /// <summary>
@@ -55,14 +55,14 @@ namespace Randomizer
         /// </summary>
         /// <param name="val">The maximum value (exclusive).</param>
         /// <returns>A random integer less than the specified value.</returns>
-        public static int GetRandomIntegerLessThanOrEqualTo(int val)
+        public static long GetRandomLongLessThanOrEqualTo(long val)
         {
-            if (val == Int32.MinValue)
+            if (val == Int64.MinValue)
             {
-                throw new ArgumentOutOfRangeException("val", "value must be greater than MinValue of Int32");
+                throw new ArgumentOutOfRangeException("val", "value must be greater than MinValue of Int64");
             }
             Random r = new Random();
-            return r.Next(Int32.MinValue, val);
+            return r.NextInt64(Int64.MinValue, val);
         }
     }
 }
